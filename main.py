ed268c04a11f6c78013b593d15e7704f86b02cf0
Marco <costa.1691388@studenti.uniroma1.it>
2020-05-16 22:12:46 +0200

    Fix

--- main.py
+++ main.py
@@ -22,9 +22,7 @@ from time import time
 
 # Change pretrained model download directory, so it doesn't
 # download every time the runtime restarts
-os.environ['TORCH_HOME'] = './models/pretrained'
-
-
+os.environ["TORCH_HOME"] = "./models/pretrained"
 
 class kWTA(nn.Module):
     def __init__(self, sr):
@@ -54,51 +52,51 @@ def activation_to_kwta(model, old_activation, sr=0.2):
 # - relu: Fine-tuned models for CIFAR10
 # - kwta_0_1: Models using kwta activation with sparsity=0.1 for CIFAR10
 # - kwta_0_2: Models using kwta activation with sparsity=0.2 for CIFAR10
-models = {'pretrained': {}, 'relu': {}, 'kwta-0.1': {}, 'kwta-0.2': {}}
+models = {"pretrained": {}, "relu": {}, "kwta-0.1": {}, "kwta-0.2": {}}
 
 # Download and load pretrained models (trained for ImageNet dataset)
-models['pretrained']['resnet'] = torchvision.models.resnet18(pretrained=True)
-models['pretrained']['alexnet'] = torchvision.models.alexnet(pretrained=True)
-#models['pretrained']['densenet'] = torchvision.models.densenet121(pretrained=True)
-#models['pretrained']['wide_resnet'] = torchvision.models.wide_resnet50_2(pretrained=True)
-#models['pretrained']['vgg'] = torchvision.models.vgg11(pretrained=True)
-#models['pretrained']['squeezenet'] = torchvision.models.squeezenet1_1(pretrained=True)
+models["pretrained"]["resnet"] = torchvision.models.resnet18(pretrained=True)
+models["pretrained"]["alexnet"] = torchvision.models.alexnet(pretrained=True)
+#models["pretrained"]["densenet"] = torchvision.models.densenet121(pretrained=True)
+#models["pretrained"]["wide_resnet"] = torchvision.models.wide_resnet50_2(pretrained=True)
+#models["pretrained"]["vgg"] = torchvision.models.vgg11(pretrained=True)
+#models["pretrained"]["squeezenet"] = torchvision.models.squeezenet1_1(pretrained=True)
 
 ## ResNet ReLU
-models['relu']['resnet'] = copy.deepcopy(models['pretrained']['resnet'])
-models['relu']['resnet'].fc.out_features = 10
-if os.path.isfile('./models/relu/ResNet18.pth'):
-    models['relu']['resnet'].load_state_dict(torch.load('./models/relu/ResNet18.pth'))
+models["relu"]["resnet"] = copy.deepcopy(models["pretrained"]["resnet"])
+models["relu"]["resnet"].fc.out_features = 10
+if os.path.isfile("./models/relu/ResNet18.pth"):
+    models["relu"]["resnet"].load_state_dict(torch.load("./models/relu/ResNet18.pth"))
 
 ## ResNet kWTA 0.1
-models['kwta-0.1']['resnet'] = copy.deepcopy(models['relu']['resnet'])
-activation_to_kwta(models['kwta-0.1']['resnet'], kWTA, sr=0.1)
-if os.path.isfile('./models/kwta-0.1/ResNet18.pth'):
-    models['kwta-0.1']['resnet'].load_state_dict(torch.load('./models/kwta-0.1/ResNet18.pth'))
+models["kwta-0.1"]["resnet"] = copy.deepcopy(models["relu"]["resnet"])
+activation_to_kwta(models["kwta-0.1"]["resnet"], kWTA, sr=0.1)
+if os.path.isfile("./models/kwta-0.1/ResNet18.pth"):
+    models["kwta-0.1"]["resnet"].load_state_dict(torch.load("./models/kwta-0.1/ResNet18.pth"))
 
 ## ResNet kWTA 0.2
-models['kwta-0.2']['resnet'] = copy.deepcopy(models['kwta-0.1']['resnet'])
-activation_to_kwta(models['kwta-0.2']['resnet'], nn.ReLU, sr=0.2)
-if os.path.isfile('./models/kwta-0.2/ResNet18.pth'):
-    models['kwta-0.2']['resnet'].load_state_dict(torch.load('./models/kwta-0.2/ResNet18.pth'))
+models["kwta-0.2"]["resnet"] = copy.deepcopy(models["kwta-0.1"]["resnet"])
+activation_to_kwta(models["kwta-0.2"]["resnet"], nn.ReLU, sr=0.2)
+if os.path.isfile("./models/kwta-0.2/ResNet18.pth"):
+    models["kwta-0.2"]["resnet"].load_state_dict(torch.load("./models/kwta-0.2/ResNet18.pth"))
 
 ## AlexNet ReLU
-models['relu']['alexnet'] = copy.deepcopy(models['pretrained']['alexnet'])
-models['relu']['alexnet'].classifier[-1].out_features = 10
-if os.path.isfile('./models/relu/AlexNet.pth'):
-    models['relu']['alexnet'].load_state_dict(torch.load('./models/relu/AlexNet.pth'))
+models["relu"]["alexnet"] = copy.deepcopy(models["pretrained"]["alexnet"])
+models["relu"]["alexnet"].classifier[-1].out_features = 10
+if os.path.isfile("./models/relu/AlexNet.pth"):
+    models["relu"]["alexnet"].load_state_dict(torch.load("./models/relu/AlexNet.pth"))
 
 ## AlexNet kWTA 0.1
-models['kwta-0.1']['alexnet'] = copy.deepcopy(models['relu']['alexnet'])
-activation_to_kwta(models['kwta-0.1']['alexnet'], kWTA, sr=0.1)
-if os.path.isfile('./models/kwta-0.1/AlexNet.pth'):
-    models['kwta-0.1']['alexnet'].load_state_dict(torch.load('./models/kwta-0.1/AlexNet.pth'))
+models["kwta-0.1"]["alexnet"] = copy.deepcopy(models["relu"]["alexnet"])
+activation_to_kwta(models["kwta-0.1"]["alexnet"], kWTA, sr=0.1)
+if os.path.isfile("./models/kwta-0.1/AlexNet.pth"):
+    models["kwta-0.1"]["alexnet"].load_state_dict(torch.load("./models/kwta-0.1/AlexNet.pth"))
 
 ## AlexNet kWTA 0.2
-models['kwta-0.2']['alexnet'] = copy.deepcopy(models['kwta-0.1']['alexnet'])
-activation_to_kwta(models['kwta-0.2']['alexnet'], nn.ReLU, sr=0.2)
-if os.path.isfile('./models/kwta-0.2/AlexNet.pth'):
-    models['kwta-0.2']['alexnet'].load_state_dict(torch.load('./models/kwta-0.2/AlexNet.pth'))
+models["kwta-0.2"]["alexnet"] = copy.deepcopy(models["kwta-0.1"]["alexnet"])
+activation_to_kwta(models["kwta-0.2"]["alexnet"], nn.ReLU, sr=0.2)
+if os.path.isfile("./models/kwta-0.2/AlexNet.pth"):
+    models["kwta-0.2"]["alexnet"].load_state_dict(torch.load("./models/kwta-0.2/AlexNet.pth"))
 
 MEAN = 0
 VAR = 1
@@ -149,8 +147,8 @@ def train(model, save_path, lr=0.1, epochs=1, batchSize=64):
          T.ToTensor(),
          T.Normalize((MEAN,MEAN,MEAN), (VAR,VAR,VAR))])
 
-    trainset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
-    testset = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
+    trainset = datasets.CIFAR10(root="./data", train=True, download=True, transform=transform_train)
+    testset = datasets.CIFAR10(root="./data", train=False, download=True, transform=transform_test)
 
     trainloader = DataLoader(trainset, batch_size=batchSize, shuffle=True)
     testloader = DataLoader(testset, batch_size=batchSize, shuffle=False)
@@ -167,15 +165,13 @@ def train(model, save_path, lr=0.1, epochs=1, batchSize=64):
         train_acc, train_err, train_loss = performEpoch(trainloader, model, optimizer, device=device, use_tqdm=True)
         print("[TRAIN] Epoch:", epoch + 1, ", Accuracy:", train_acc, ", Error:", train_err, ", Loss:", train_loss)
 
-        test_acc, test_err, test_loss = performEpoch(testloader, model, device=device, use_tqdm=True)
-        print("[TEST] Epoch:", epoch + 1, ", Accuracy:", test_acc, ", Error:", test_err, ", Loss:", test_loss)
-
         # save checkpoint after every epoch
         torch.save(model.state_dict(), save_path)
 
-    print('Finished Training')
+        test_acc, test_err, test_loss = performEpoch(testloader, model, device=device, use_tqdm=True)
+        print("[TEST] Epoch:", epoch + 1, ", Accuracy:", test_acc, ", Error:", test_err, ", Loss:", test_loss)
 
     end = time()
-    print("Time Elapsed=%d" % (end - start))
+    print("Finished Training. Time Elapsed=%d" % (end - start))
 
-train(models['relu']['resnet'], lr=0.01, epochs=2, save_path='./models/relu/ResNet18.pth', batchSize=32)
+train(models["relu"]["resnet"], lr=0.01, epochs=2, save_path="./models/relu/ResNet18.pth", batchSize=32)
