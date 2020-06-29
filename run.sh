631b80afee8dd0939481d36b12fb51ab9ac572b6
Marco <costa.1691388@studenti.uniroma1.it>
2020-06-29 23:58:21 +0200

    Updated

--- run.sh
+++ run.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
 # Train nets
-#python main.py --task train --model ResNet18 --dataset CIFAR-10 --batchSize 64 --activation ReLU --pretrained True --epochs 10 --lr 0.01
-#python main.py --task train --model ResNet18 --dataset CIFAR-10 --batchSize 64 --activation k-WTA --kWTAsr 0.2 --pretrained True --epochs 10 --lr 0.01
-#python main.py --task train --model ResNet18 --dataset CIFAR-10 --batchSize 64 --activation k-WTA --kWTAsr 0.1 --pretrained True --epochs 10 --lr 0.01
-#python main.py --task train --model ResNet18 --dataset CIFAR-10 --batchSize 64 --activation ReLU --lr 0.1
-#python main.py --task train --model ResNet18 --dataset CIFAR-10 --batchSize 64 --activation k-WTA --kWTAsr 0.2 --lr 0.1
-#python main.py --task train --model ResNet18 --dataset CIFAR-10 --batchSize 64 --activation k-WTA --kWTAsr 0.1 --lr 0.1
+python main.py --task train --model ResNet18 --dataset CIFAR-10 --batchSize 64 --activation ReLU --pretrained True --epochs 10 --lr 0.01
+python main.py --task train --model ResNet18 --dataset CIFAR-10 --batchSize 64 --activation k-WTA --kWTAsr 0.2 --pretrained True --epochs 10 --lr 0.01
+python main.py --task train --model ResNet18 --dataset CIFAR-10 --batchSize 64 --activation k-WTA --kWTAsr 0.1 --pretrained True --epochs 10 --lr 0.01
+python main.py --task train --model ResNet18 --dataset CIFAR-10 --batchSize 64 --activation ReLU --lr 0.1
+python main.py --task train --model ResNet18 --dataset CIFAR-10 --batchSize 64 --activation k-WTA --kWTAsr 0.2 --lr 0.1
+python main.py --task train --model ResNet18 --dataset CIFAR-10 --batchSize 64 --activation k-WTA --kWTAsr 0.1 --lr 0.1
 
-#python main.py --task train --model ResNet18 --dataset SVHN --batchSize 64 --activation ReLU --pretrained True --epochs 10 --lr 0.01
-#python main.py --task train --model ResNet18 --dataset SVHN --batchSize 64 --activation k-WTA --kWTAsr 0.2 --pretrained True --epochs 10 --lr 0.01
-#python main.py --task train --model ResNet18 --dataset SVHN --batchSize 64 --activation k-WTA --kWTAsr 0.1 --pretrained True --epochs 10 --lr 0.01
-#python main.py --task train --model ResNet18 --dataset SVHN --batchSize 64 --activation ReLU --lr 0.1
-#python main.py --task train --model ResNet18 --dataset SVHN --batchSize 64 --activation k-WTA --kWTAsr 0.2 --lr 0.1
-#python main.py --task train --model ResNet18 --dataset SVHN --batchSize 64 --activation k-WTA --kWTAsr 0.1 --lr 0.1
+python main.py --task train --model ResNet18 --dataset SVHN --batchSize 64 --activation ReLU --pretrained True --epochs 10 --lr 0.01
+python main.py --task train --model ResNet18 --dataset SVHN --batchSize 64 --activation k-WTA --kWTAsr 0.2 --pretrained True --epochs 10 --lr 0.01
+python main.py --task train --model ResNet18 --dataset SVHN --batchSize 64 --activation k-WTA --kWTAsr 0.1 --pretrained True --epochs 10 --lr 0.01
+python main.py --task train --model ResNet18 --dataset SVHN --batchSize 64 --activation ReLU --lr 0.1
+python main.py --task train --model ResNet18 --dataset SVHN --batchSize 64 --activation k-WTA --kWTAsr 0.2 --lr 0.1
+python main.py --task train --model ResNet18 --dataset SVHN --batchSize 64 --activation k-WTA --kWTAsr 0.1 --lr 0.1
 
-#python main.py --task train --model AlexNet --dataset CIFAR-10 --batchSize 64 --activation ReLU --pretrained True --epochs 10 --lr 0.01
-#python main.py --task train --model AlexNet --dataset CIFAR-10 --batchSize 64 --activation k-WTA --kWTAsr 0.2 --pretrained True --epochs 10 --lr 0.01
-#python main.py --task train --model AlexNet --dataset CIFAR-10 --batchSize 64 --activation k-WTA --kWTAsr 0.1 --pretrained True --epochs 10 --lr 0.01
-#python main.py --task train --model AlexNet --dataset CIFAR-10 --batchSize 64 --activation ReLU --lr 0.1
-#python main.py --task train --model AlexNet --dataset CIFAR-10 --batchSize 64 --activation k-WTA --kWTAsr 0.2 --lr 0.1
-#python main.py --task train --model AlexNet --dataset CIFAR-10 --batchSize 64 --activation k-WTA --kWTAsr 0.1 --lr 0.1
+python main.py --task train --model AlexNet --dataset CIFAR-10 --batchSize 64 --activation ReLU --pretrained True --epochs 10 --lr 0.01
+python main.py --task train --model AlexNet --dataset CIFAR-10 --batchSize 64 --activation k-WTA --kWTAsr 0.2 --pretrained True --epochs 10 --lr 0.01
+python main.py --task train --model AlexNet --dataset CIFAR-10 --batchSize 64 --activation k-WTA --kWTAsr 0.1 --pretrained True --epochs 10 --lr 0.01
+python main.py --task train --model AlexNet --dataset CIFAR-10 --batchSize 64 --activation ReLU --lr 0.1
+python main.py --task train --model AlexNet --dataset CIFAR-10 --batchSize 64 --activation k-WTA --kWTAsr 0.2 --lr 0.1
+python main.py --task train --model AlexNet --dataset CIFAR-10 --batchSize 64 --activation k-WTA --kWTAsr 0.1 --lr 0.1
 
-#python main.py --task train --model AlexNet --dataset SVHN --batchSize 64 --activation ReLU --pretrained True --epochs 10 --lr 0.01
-#python main.py --task train --model AlexNet --dataset SVHN --batchSize 64 --activation k-WTA --kWTAsr 0.2 --pretrained True --epochs 10 --lr 0.01
-#python main.py --task train --model AlexNet --dataset SVHN --batchSize 64 --activation k-WTA --kWTAsr 0.1 --pretrained True --epochs 10 --lr 0.01
-#python main.py --task train --model AlexNet --dataset SVHN --batchSize 64 --activation ReLU --lr 0.1
-#python main.py --task train --model AlexNet --dataset SVHN --batchSize 64 --activation k-WTA --kWTAsr 0.2 --lr 0.1
-#python main.py --task train --model AlexNet --dataset SVHN --batchSize 64 --activation k-WTA --kWTAsr 0.1 --lr 0.1
+python main.py --task train --model AlexNet --dataset SVHN --batchSize 64 --activation ReLU --pretrained True --epochs 10 --lr 0.01
+python main.py --task train --model AlexNet --dataset SVHN --batchSize 64 --activation k-WTA --kWTAsr 0.2 --pretrained True --epochs 10 --lr 0.01
+python main.py --task train --model AlexNet --dataset SVHN --batchSize 64 --activation k-WTA --kWTAsr 0.1 --pretrained True --epochs 10 --lr 0.01
+python main.py --task train --model AlexNet --dataset SVHN --batchSize 64 --activation ReLU --lr 0.1
+python main.py --task train --model AlexNet --dataset SVHN --batchSize 64 --activation k-WTA --kWTAsr 0.2 --lr 0.1
+python main.py --task train --model AlexNet --dataset SVHN --batchSize 64 --activation k-WTA --kWTAsr 0.1 --lr 0.1
 
 # Test Adversarial
 python main.py --task attack --model ResNet18 --dataset CIFAR-10 --batchSize 64 --activation ReLU --attack PGD --batches 20 --loadModel True
